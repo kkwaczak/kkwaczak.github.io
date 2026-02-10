c7ad6aad3603e09fc807eff95738e4f19f3ba88d
kkwaczak <kkwaczak@gmail.com>
2026-02-10 09:40:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /**
  * KONFIGURACJA UKŁADU WSPÓŁRZĘDNYCH (EPSG:2178 - Układ Warszawski)
  */
-const warstwaKonfig = {
+var warstwaKonfig = {
     "minX": 7340000.0, "minY": 5700000.0, "maxX": 7670000.0, "maxY": 5890000.0,
     "zoomLevels": [
         { "tileWidth": 69358.93333333332 }, { "tileWidth": 34679.46666666666 },
@@ -17,8 +17,8 @@ const warstwaKonfig = {
     ]
 };
 
-const resolutions = warstwaKonfig.zoomLevels.map(l => l.tileWidth / 256);
-const crs2178 = new L.Proj.CRS('EPSG:2178', "+proj=tmerc +lat_0=0 +lon_0=21 +k=0.999923 +x_0=7500000 +y_0=0 +ellps=GRS80 +units=m +no_defs", {
+var resolutions = warstwaKonfig.zoomLevels.map(function(l) { return l.tileWidth / 256; });
+var crs2178 = new L.Proj.CRS('EPSG:2178', "+proj=tmerc +lat_0=0 +lon_0=21 +k=0.999923 +x_0=7500000 +y_0=0 +ellps=GRS80 +units=m +no_defs", {
     resolutions: resolutions,
     origin: [warstwaKonfig.minX, warstwaKonfig.minY]
 });
@@ -28,48 +28,46 @@ const crs2178 = new L.Proj.CRS('EPSG:2178', "+proj=tmerc +lat_0=0 +lon_0=21 +k=0
  */
 L.PodkladWarszawski = L.TileLayer.extend({
     getTileUrl: function(coords) {
-        const x = coords.x;
-        const y = -coords.y - 1;
-        const zoom = coords.z;
+        var x = coords.x;
+        var y = -coords.y - 1;
+        var zoom = coords.z;
         if (y < 0 || x < 0) return "";
-        return `https://testmapa.um.warszawa.pl/mapviewer/mcserver?request=gettile&format=PNG&zoomlevel=${zoom}&mapcache=${this.options.mapname}&mx=${x}&my=${y}`;
+        return "https://testmapa.um.warszawa.pl/mapviewer/mcserver?request=gettile&format=PNG&zoomlevel=" + zoom + "&mapcache=" + this.options.mapname + "&mx=" + x + "&my=" + y;
     }
 });
 
-const mapOptions = { minZoom: 0, maxZoom: resolutions.length - 1, noWrap: true };
+var mapOptions = { minZoom: 0, maxZoom: resolutions.length - 1, noWrap: true };
 
 // Warstwy kafelkowe
-const baseMaps = {
-    "Lindley (Plan S)": new L.PodkladWarszawski('', { ...mapOptions, mapname: "DANE_WAWA.LINDLEY_2500_S" }),
-    "Plan 1936": new L.PodkladWarszawski('', { ...mapOptions, mapname: "DANE_WAWA.PLAN_1936" }),
-    "Plan BOS": new L.PodkladWarszawski('', { ...mapOptions, mapname: "DANE_WAWA.PLAN_BOS" }),
-    "Wektor": new L.PodkladWarszawski('', { ...mapOptions, mapname: "DANE_WAWA.WARSZAWA_PODKLAD_WEKTOR" })
+var baseMaps = {
+    "Lindley (Plan S)": new L.PodkladWarszawski('', { minZoom: 0, maxZoom: 18, noWrap: true, mapname: "DANE_WAWA.LINDLEY_2500_S" }),
+    "Plan 1936": new L.PodkladWarszawski('', { minZoom: 0, maxZoom: 18, noWrap: true, mapname: "DANE_WAWA.PLAN_1936" }),
+    "Plan BOS": new L.PodkladWarszawski('', { minZoom: 0, maxZoom: 18, noWrap: true, mapname: "DANE_WAWA.PLAN_BOS" }),
+    "Wektor": new L.PodkladWarszawski('', { minZoom: 0, maxZoom: 18, noWrap: true, mapname: "DANE_WAWA.WARSZAWA_PODKLAD_WEKTOR" })
 };
 
 /**
  * INICJALIZACJA MAPY
  */
-const map = L.map('map', {
+var map = L.map('map', {
     crs: crs2178,
     layers: [baseMaps["Lindley (Plan S)"]]
 }).setView([52.2210, 21.0150], 16);
 
 L.control.layers(baseMaps).setPosition("bottomleft").addTo(map);
 
-const markersLayer = new L.LayerGroup().addTo(map);
+var markersLayer = new L.LayerGroup().addTo(map);
 
 /**
  * FUNKCJE OBSŁUGI PUNKTÓW
  */
 
-// Funkcja przybliżania do punktu po kliknięciu w tabelę
 window.zoomToPoint = function(lat, lon) {
     map.setView([lat, lon], 17);
 };
 
-// Dodawanie znacznika do mapy
 function addMarkerToMap(hip, lat, lon) {
-    const marker = L.circleMarker([lat, lon], {
+    var marker = L.circleMarker([lat, lon], {
         radius: 6,
         color: '#ffffff',
         fillColor: '#007bff',
@@ -77,29 +75,24 @@ function addMarkerToMap(hip, lat, lon) {
         weight: 2
     });
     
-    marker.options.title = hip; // Potrzebne dla wyszukiwarki
-    marker.bindPopup(`<strong>Numer HIP: ${hip}</strong><br>Współrzędne: ${lat.toFixed(5)}, ${lon.toFixed(5)}`);
+    marker.options.title = hip.toString(); 
+    marker.bindPopup("<strong>Numer HIP: " + hip + "</strong><br>Współrzędne: " + lat.toFixed(5) + ", " + lon.toFixed(5));
     markersLayer.addLayer(marker);
 }
 
-// Renderowanie tabeli w bocznym panelu
 function renderTable(points) {
-    const tbody = document.querySelector('#points-table tbody');
+    var tbody = document.querySelector('#points-table tbody');
     if (!tbody) return;
 
-    // Sortowanie alfanumeryczne (np. 1, 2, 10, 100 zamiast 1, 10, 100, 2)
-    points.sort((a, b) => a.hip.toString().localeCompare(b.hip.toString(), undefined, { numeric: true }));
+    points.sort(function(a, b) { 
+        return a.hip.toString().localeCompare(b.hip.toString(), undefined, { numeric: true }); 
+    });
 
-    tbody.innerHTML = ''; // Czyścimy tabelę przed renderowaniem
-    points.forEach(item => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>
-                <a href="#" class="point-link" onclick="zoomToPoint(${item.gps.lat}, ${item.gps.lon}); return false;">
-                    ${item.hip}
-                </a>
-            </td>
-        `;
+    tbody.innerHTML = ''; 
+    points.forEach(function(item) {
+        var row = document.createElement('tr');
+        var link = '<a href="#" class="point-link" onclick="zoomToPoint(' + item.gps.lat + ', ' + item.gps.lon + '); return false;">' + item.hip + '</a>';
+        row.innerHTML = '<td>' + link + '</td>';
         tbody.appendChild(row);
     });
 }
@@ -107,14 +100,14 @@ function renderTable(points) {
 /**
  * POBIERANIE DANYCH Z PLIKU hip.txt
  */
-fetch('./hip.txt')
-    .then(response => {
-        if (!response.ok) throw new Error('Nie udało się wczytać pliku hip.txt');
+fetch('hip.txt')
+    .then(function(response) {
+        if (!response.ok) throw new Error('Błąd HTTP: ' + response.status);
         return response.json();
     })
-    .then(data => {
+    .then(function(data) {
         if (data && data.hipy) {
-            data.hipy.forEach(item => {
+            data.hipy.forEach(function(item) {
                 if(item.gps && item.gps.lat && item.gps.lon) {
                     addMarkerToMap(item.hip, item.gps.lat, item.gps.lon);
                 }
@@ -122,18 +115,18 @@ fetch('./hip.txt')
             renderTable(data.hipy);
         }
     })
-    .catch(error => {
+    .catch(function(error) {
         console.error('Błąd:', error);
-        const tbody = document.querySelector('#points-table tbody');
-        if (tbody) tbody.innerHTML = `<tr><td style="color:red">Błąd ładowania danych</td></tr>`;
+        var tbody = document.querySelector('#points-table tbody');
+        if (tbody) tbody.innerHTML = '<tr><td style="color:red">Blad danych: ' + error.message + '</td></tr>';
     });
 
 /**
  * KONFIGURACJA WYSZUKIWARKI
  */
-const searchControl = new L.Control.Search({
+var searchControl = new L.Control.Search({
     layer: markersLayer,
-    propertyName: 'title', // Wyszukuje po tytule markera (numerze HIP)
+    propertyName: 'title',
     initial: false,
     zoom: 18,
     marker: false,
